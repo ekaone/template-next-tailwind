f9d8bb6fa4c30327a1947635ff8fb0750f4b0a53
Eka Prasetia <ekaone3033@gmail.com>
2023-04-29 21:22:20 +0700

    add background grid

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,34 @@
 /** @type {import('tailwindcss').Config} */
+
+import colors from "tailwindcss/colors";
+
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: "class",
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      backgroundImage: ({ theme }) => ({
+        "vc-border-gradient": `radial-gradient(at left top, ${theme(
+          "colors.gray.500"
+        )}, 50px, ${theme("colors.gray.800")} 50%)`,
+      }),
+    },
+    colors: {
+      gray: colors.zinc,
+      "gray-1000": "rgb(17,17,19)",
+      "gray-1100": "rgb(10,10,11)",
+      vercel: {
+        pink: "#FF0080",
+        blue: "#0070F3",
+        cyan: "#50E3C2",
+        orange: "#F5A623",
+        violet: "#7928CA",
       },
     },
   },
   plugins: [],
-}
+};
